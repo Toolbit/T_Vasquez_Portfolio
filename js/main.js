692bea5410498a6a8b2188c54d49729cf0e12373
tvasquez <tdt-s@cox.net>
2025-06-25 16:03:11 -0700

    Upload files fro Download Sharing Page

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const files = [
+  {
+    name: "Travis Vasquez Portfolio",
+    path: "VIDEO/T_Vasquez_Portfolio.mp4"
+  },
+  {
+    name: "Concord Trailor Teaser",
+    path: "VIDEO/Concord - Teaser Trailer PS5 PC Games (1080p).mp4"
+  },
+  {
+    name: "Travis Vasquez CV",
+    path: "assets/Vasquez_T_CV_2025.pdf"
+  }
+];
+
+const container = document.getElementById('file-list');
+
+files.forEach(file => {
+  const link = document.createElement('a');
+  link.href = file.path;
+  link.download = '';
+  link.textContent = `Download: ${file.name}`;
+  link.className = 'download-link';
+
+  const div = document.createElement('div');
+  div.appendChild(link);
+
+  container.appendChild(div);
+});
